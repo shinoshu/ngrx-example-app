5bbedfaecfb7f4b85717a4c5e0015ef80c272850
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-15 17:31:42 +0900

    ng generate component login-form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './containers/login-page/login-page.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 
 
 @NgModule({
-  declarations: [LoginPageComponent],
+  declarations: [LoginPageComponent, LoginFormComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
