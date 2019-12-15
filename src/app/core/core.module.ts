11901e1f93c0f58b4a6a22de9974ff3864a29da1
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-15 13:51:13 +0900

    update core.module.ts

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
+import { MaterialModule } from '../material';
+import {
+  LayoutComponent,
+  NavItemComponent,
+  SidenavComponent,
+  ToolbarComponent,
+} from './components';
+import { AppComponent } from './containers';
 
+export const COMPONENTS = [
+  AppComponent,
+  LayoutComponent,
+  NavItemComponent,
+  SidenavComponent,
+  ToolbarComponent,
+];
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule,
+    MaterialModule,
+  ],
+  declarations: COMPONENTS,
+  exports: COMPONENTS,
 })
 export class CoreModule { }
