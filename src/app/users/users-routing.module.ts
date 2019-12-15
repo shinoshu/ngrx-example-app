f3c26a986ab680f520254159dac6a6848edd9174
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-15 18:38:20 +0900

    ng generate module users-routing --flat --module=users

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UsersRoutingModule } from './users-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    UsersRoutingModule
+    CommonModule
   ]
 })
-export class UsersModule { }
+export class UsersRoutingModule { }
