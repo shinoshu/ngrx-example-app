18b1cb7d536de2de4b1787bf085da74af917b1dd
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-15 21:32:40 +0900

    ng generate component user-table

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { CollectionPageComponent } from './containers/collection-page/collection-page.component';
+import { UserTableComponent } from './components/user-table/user-table.component';
 
 
 
 @NgModule({
-  declarations: [CollectionPageComponent],
+  declarations: [CollectionPageComponent, UserTableComponent],
   imports: [
     CommonModule,
     UsersRoutingModule
