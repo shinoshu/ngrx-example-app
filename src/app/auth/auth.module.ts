9c22ed2bac2acbb96aa07c3a943fa629c2bcf98f
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-15 17:29:44 +0900

    ng generate component login-page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginPageComponent } from './containers/login-page/login-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginPageComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
