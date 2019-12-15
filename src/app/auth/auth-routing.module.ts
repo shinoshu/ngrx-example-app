1214c130a446a61b5af9305c029bd005d2083d87
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-15 17:28:05 +0900

    ng generate module auth-routing --flat --module=auth

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthRoutingModule } from './auth-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    AuthRoutingModule
+    CommonModule
   ]
 })
-export class AuthModule { }
+export class AuthRoutingModule { }
