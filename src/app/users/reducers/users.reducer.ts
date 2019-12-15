8541ed4c2b4f653155d5daba49be3596d07d3b4a
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-16 00:00:35 +0900

    update users.reducer.ts

--- src/app/users/reducers/users.reducer.ts
+++ src/app/users/reducers/users.reducer.ts
@@ -1,5 +1,7 @@
 import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
 import { createReducer, on } from '@ngrx/store';
+
+import { addUser } from '../actions/user.actions';
 import { User } from '../models/user';
 
 export const usersFeatureKey = 'users';
@@ -9,3 +11,8 @@ export interface State extends EntityState<User> { }
 export const adapter: EntityAdapter<User> = createEntityAdapter<User>();
 
 export const initialState: State = adapter.getInitialState();
+
+export const reducer = createReducer(
+  initialState,
+  on(addUser, (state, { user }) => adapter.addOne(user, state)),
+);
